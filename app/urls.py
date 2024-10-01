535d4d8e032b9bdff07ce0f69712842034b0172b
bugston <bugstonn@gmail.com>
2024-10-01 15:51:12 +0300

    s

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import store
+from .views import store, add_to_cart
 
 urlpatterns = [
     path('', store, name='store'),
+    path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Product
+from django.http import HttpResponse
+
+class Cart:
+    def __init__(self):
+        self.items = {}
+
+    def add(self, product_id):
+        if product_id in self.items:
+            self.items[product_id] += 1
+        else:
+            self.items[product_id] = 1
+
+    def get_items(self):
+        return [{'product': get_object_or_404(Product, id=pid), 'quantity': qty} for pid, qty in self.items.items()]
+
+cart = Cart()
 
 def store(request):
     products = Product.objects.all()
-    return render(request, 'store.html', {'products': products})
+    cart_items = cart.get_items()
+    return render(request, 'store.html', {'products': products, 'cart_items': cart_items})
+
+def add_to_cart(request, product_id):
+    cart.add(product_id)
+    return redirect('store')
